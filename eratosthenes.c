096309906bd852ae30cdc01e3e878e9ce7424ef0
shevapav <pabut@LAPTOP-D0CL22KB.localdomain>
2023-02-27 00:18:08 +0100

    Encoding is added

--- eratosthenes.c
+++ eratosthenes.c
@@ -3,11 +3,11 @@
 #include <assert.h>
 #include <time.h>
 #include "math.h"
-#include "ppm.h"
+#include "ppm.c"
 
 
-void eratosthenes(unsigned long N){
-    bitset_t arr = 0;
+bitset_t eratosthenes(unsigned long N){
+    bitset_t arr;
     bitset_create(&arr, N + 1);
     bitset_setbit(arr, 0, 1);
     bitset_setbit(arr, 1, 1);
@@ -18,6 +18,13 @@ void eratosthenes(unsigned long N){
             }
         }
     }
+    return arr;
+}
+
+/*int main(){
+    clock_t start = clock();
+    unsigned long N = 230000000LU;
+    bitset_t arr = eratosthenes(N);
     int count = 0;
     for(unsigned long i = N; i > 0 && count < 10; i--){
         if(bitset_getbit(arr, i) == 0){
@@ -26,11 +33,57 @@ void eratosthenes(unsigned long N){
         }
     }
     bitset_free(arr);
-}
+    fprintf(stderr, "Time=%.3g\n", (double)(clock()-start)/CLOCKS_PER_SEC);
+    return 0;
+}*/
 
-int main(){
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        error_exit("main: Invalid amount of arguments, name of file is not specified");
+    }
     clock_t start = clock();
-    eratosthenes(230000000LU);
+    struct ppm* image = ppm_read(argv[1]);
+    unsigned long N = 3 * image->xsize * image->ysize;
+    bitset_t arr = eratosthenes(N);
+    int primary_count = 0;
+    
+    
+    for(unsigned long i = 101; i < N; i++){
+        if(bitset_getbit(arr, i) == 0){
+            primary_count++;
+        }
+    }
+
+
+    int bit_pos = 0;
+    int str_pos = 0;
+    char c = 0;
+    int g = 0;
+    char* str = malloc(primary_count * sizeof(char) / 8);
+    for(unsigned long i = 101; i < N; i++){
+        if(bitset_getbit(arr, i) == 0){
+            g = image->data[i] & 1;
+            if(g == 0){
+                c &= ~(g << bit_pos++);
+            }
+            else{
+                c |= g << bit_pos++;
+            }
+            if(bit_pos == 8){
+                bit_pos = 0;
+                str[str_pos++] = c;
+                if(c == '\0'){
+                    break;  
+                }
+                c = 0;
+            }
+
+        }
+    }
+    printf("\n%s\n", str);
+    
+    bitset_free(arr);
+    ppm_free(image);
     fprintf(stderr, "Time=%.3g\n", (double)(clock()-start)/CLOCKS_PER_SEC);
     return 0;
 }
--- ppm.c
+++ ppm.c
@@ -1,48 +1,55 @@
 #include "ppm.h"
 #include <stdlib.h>
-#include "error.c"
+//#include "error.c"
 
 #define COORDINATE_LIMIT 16000
 
 struct ppm* ppm_read(const char * filename){
     FILE *file;
-    file = fopen(filename);
+    file = fopen(filename, "r");
     unsigned x,y, color_depth;
     if(file == NULL){
-        warning("ppm_read: Could\'t open the file");
+        // warning("ppm_read: Could\'t open the file");
+        return NULL;
     }
     if(getc(file) != 'P' || getc(file) != '6' ||
      fscanf(file, "%u %u", &x, &y) != 2 ||
      fscanf(file, "%u", &color_depth) != 1 ||
      color_depth != 255){
-        warning("ppm_read: Wrong file format");
+        //warning("ppm_read: Wrong file format");
+        fclose(file);
         return NULL;
     }
+    //symbol \n
+    getc(file);
     if(x > COORDINATE_LIMIT || y > COORDINATE_LIMIT){
-        warning("ppm_read: %u is out allowed range(%u)", x > COORDINATE_LIMIT ? x : y, (unsigned)COORDINATE_LIMIT);
+        // warning("ppm_read: %u is out allowed range(%u)", x > COORDINATE_LIMIT ? x : y, (unsigned)COORDINATE_LIMIT);
+        fclose(file);
+        return NULL;
     }
-    struct ppm* obj = (struct ppm*)malloc(sizeof(struct ppm));
+    struct ppm* obj = (struct ppm*)malloc(sizeof(struct ppm) + sizeof(char) * 3 * x * y);
     if(obj == NULL){
-        warning("ppm_read: Memory allocation error");
+        // warning("ppm_read: Memory allocation error");
+        fclose(file);
         return NULL;
     }
     obj->xsize = x;
     obj->ysize = y;
-    obj->data = (char*)malloc(3 * x * y);
     if(obj->data == NULL){
-        warning("ppm_read: Memory allocation error");
+        // warning("ppm_read: Memory allocation error");
         free(obj);
+        fclose(file);
         return NULL;
     }
     char c;
     for(int i = 0; (c = getc(file)) != EOF; i++){
         obj->data[i] = c;
     }
+    fclose(file);
 
     return obj;
 }
 
 void ppm_free(struct ppm *p){
-    free(p->data);
     free(p);
 }
